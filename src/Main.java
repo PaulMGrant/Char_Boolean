a425b0d17920210857955a5f47e647bbc5836e6d
Paul M Grant <paul@paul-grant.co.uk>
2023-02-22 13:24:55 +0100

    Char

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         char myChar = '\u00A9'; /* how to use unicode char */
-
         System.out.println(myChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
     }
 }
